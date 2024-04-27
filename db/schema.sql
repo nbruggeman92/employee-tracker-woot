06a7d86cb0ca910be1b359852168e8b407cf3057
Nick Bruggeman <nick.bruggeman92@gmail.com>
2024-04-27 11:32:27 -0700

    npm installs and changes to schema

--- db/schema.sql
+++ db/schema.sql
@@ -3,4 +3,27 @@ CREATE DATABASE employees_db;
 
 \c employees_db;
 
-CREATE TABLE
+CREATE TABLE department (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(30) UNIQUE NOT NULL
+);
+
+CREATE TABLE role (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(30) UNIQUE NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INTEGER NOT NULL,
+    FOREIGN KEY (department_id)
+    REFERENCES department(id) ON DELETE CASCADE
+);
+
+CREATE TABLE employees (
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INTEGER NOT NULL,
+    FOREIGN KEY (role_id)
+    REFERENCES role(id) ON DELETE CASCADE,
+    manager_id INTEGER,
+    FOREIGN KEY (manager_id) REFERENCES manager(id) ON DELETE SET NULL
+);
